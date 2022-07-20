5e8e4a39870535e8326aa9eb305ca09204c96dff
ADMIN <ADMIN@DESKTOP-3S146JU>
2022-07-20 08:06:54 +0700

    Khoi tao file Git01.java, noi dung mac dinh

---
+++ Git01/src/main/java/vn/edu/fpt/git01/Git01.java
@@ -0,0 +1,12 @@
+package vn.edu.fpt.git01;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Git01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
